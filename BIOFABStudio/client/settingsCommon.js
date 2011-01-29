1dbb0adf942cd0e02595b292e58ff3c3354bb5bb
Cesar A. Rodriguez <one.seminole@gmail.com>
2011-01-29 13:44:22 -0800

    Commited the renamed folder.

---
+++ BIOFABStudio/client/settingsCommon.js
@@ -0,0 +1,6 @@
+
+# Global settings that are not machine-dependent
+var SERVICE_PATH_DATABASE = SERVICES_PATH + '/Database';
+var SERVICE_PATH_ANNEALER = SERVICES_PATH + '/annealer';
+var SERVICE_PATH_ASSEMBLER = SERVICES_PATH + '/assembler';
+var RBS_CALCULATE_URL = SERVICES_PATH + '/RBSCalculator/calculate';
