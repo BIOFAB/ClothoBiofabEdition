2be186d8c27b09e92ce39040a52dd331b0d061f8
Jenhan <jenhantao@gmail.com>
2011-04-03 14:34:04 -0700

    removed redundant data structures, added more getter methods in SeqCheckInput class

--- Clotho Fab Edition/ClothoApps/SequenceChecker/src/org/clothocad/tool/sequencechecker/SeqCheckInput.java
+++ Clotho Fab Edition/ClothoApps/SequenceChecker/src/org/clothocad/tool/sequencechecker/SeqCheckInput.java
@@ -5,81 +5,99 @@ import java.util.ArrayList;
 
 public class SeqCheckInput {
 
-    private String[] trace;
-    private String[] constructName;
-    private String[] cloneNumber;
-    private String[] wellNumber;
     private Object[][] table; //table form of the data
+    private int numberOfElements; //stores the number of trace files held in table
+    private ArrayList<File> files; //holds all the file objects
 
     public SeqCheckInput(ArrayList<File> fileList) {
-        trace = new String[fileList.size()];
-        constructName = new String[fileList.size()];
-        cloneNumber = new String[fileList.size()];
-        wellNumber = new String[fileList.size()];
         table = new Object[fileList.size()][4];
+        numberOfElements = fileList.size();
+        files = fileList;
         int i = 0;
         for (File file : fileList) {
-            trace[i] = file.getName();
+            table[i][0] = file.getName();
             String[] tokens = file.getName().split("[\\p{Punct}]");
-            constructName[i] = tokens[0]; //construct name should appear before first underscore or period
-            wellNumber[i] = tokens[tokens.length - 2];
+            table[i][1] = tokens[0]; //construct name should appear before first underscore or period
+            table[i][3] = tokens[tokens.length - 2];
             try {
                 Integer.parseInt(tokens[1]);
-                cloneNumber[i] = tokens[1]; //second token should contain the clone number
+                table[i][2] = tokens[1]; //second token should contain the clone number
             } catch (NumberFormatException e) {
-                cloneNumber[i] = null;
+                table[i][2] = null;
             }
-            //Maintains the table contents
-            table[i][0] = trace[i];
-            table[i][1] = constructName[i];
-            table[i][2] = cloneNumber[i];
-            table[i][3] = wellNumber[i];
-
             i++;
         }
     }
     //Iterates through contents of object, printing a 1 line summary for each trace file
 
     public void print() {
-        for (int i = 0; i < trace.length; i++) {
-            System.out.println("trace: " + trace[i] + " construct: " + constructName[i] + " cloneNumber: " + cloneNumber[i] + " wellNumber: " + wellNumber[i]);
+        for (int i = 0; i < table.length; i++) {
+            System.out.println("trace: " + table[i][0] + " construct: " + table[i][1] + " cloneNumber: " + table[i][2] + " wellNumber: " + table[i][3]);
         }
     }
     //returns the number of files in the selected directory
 
     public int getNumberOfElements() {
-        return trace.length;
+        return numberOfElements;
     }
     //returns the trace array
 
     public String[] getTraces() {
-        return trace;
+        String[] toReturn = new String[numberOfElements];
+        for (int i = 0; i < numberOfElements; i++) {
+            toReturn[i] = (String) table[i][0];
+
+        }
+        return toReturn;
     }
     //returns the constructName array
 
     public String[] getConstructNames() {
-        return constructName;
+        String[] toReturn = new String[numberOfElements];
+        for (int i = 0; i < numberOfElements; i++) {
+            toReturn[i] = (String) table[i][1];
+
+        }
+        return toReturn;
     }
     //returns the cloneNumber array
 
     public String[] getCloneNumbers() {
-        return cloneNumber;
+        String[] toReturn = new String[numberOfElements];
+        for (int i = 0; i < numberOfElements; i++) {
+            toReturn[i] = (String) table[i][2];
+
+        }
+        return toReturn;
     }
     //returns the wellNumber array
 
     public String[] getWellNumbers() {
-        return wellNumber;
+        String[] toReturn = new String[numberOfElements];
+        for (int i = 0; i < numberOfElements; i++) {
+            toReturn[i] = (String) table[i][3];
+
+        }
+        return toReturn;
+    }
+    //Returns the file paths of each trace file in table
+    public String[] getfilePaths() {
+        String[] toReturn = new String[numberOfElements];
+        int i = 0;
+        for (File file : files) {
+            toReturn[i] = file.getPath();
+            i++;
+        }
+        return toReturn;
+    }
+
+    //returns all File objects represented in the table
+    public ArrayList<File> getFiles() {
+        return files;
     }
 
     public Object[][] getTable() {
-        /*Object toReturn[][] = new Object[this.getNumberOfElements()][4];
-        for (int i = 0; i < this.getNumberOfElements(); i++) {
-            toReturn[i][0] = trace[i];
-            toReturn[i][1] = constructName[i];
-            toReturn[i][2] = cloneNumber[i];
-            toReturn[i][3] = wellNumber[i];
-        }
-        return toReturn;*/
+
         return table;
     }
 }
